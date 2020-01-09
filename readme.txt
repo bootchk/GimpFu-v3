f93501ca665fd008bc11b3eeab6208ea94671453
bootchk <bootch@nc.rr.com>
2020-01-09 08:57:23 -0500

    TODO list in readme.  pdb proved.

--- readme.txt
+++ readme.txt
@@ -12,11 +12,62 @@ to say /usr/local/lib/gimp/2.99/python/gimpfu.py
 
 Repository also includes a Vagga script to create a development container for Gimp 3.
 
+TODO list
+=========
 
-Dev environment
+This is a high-level list of features that GimpFu provides,
+that need to be ported/rewritten
+along with the status in parenthesis
+
+1. Automatic plugin GUI generation (proved concept, needs breadth)
+2. pdb convenience object (in progress)
+3. Gimp enums to Python names (no progress)
+4. PF_ enums, the ones that GimpFu/PythonFu defines (strategy devised, needs depth and breadth)
+5. Use new plugin "defaults" machinery in Gimp 3, really "plugin settings", called "gimp_procedure_config" (strategy devised, no progress)
+6. Other convenience Python objects e.g. "image" (not started, possibly obsolete them?)
+7. Write a "GimpFu plugin Porting Guide" (drafted, this TODO list is its outline)
+8. Adapt to new plugin installation requirements (proved)
+
+Dev strategy
+============
+
+True hacking.  Start coding, see what is broken, fix, and repeat.
+
+Lacking a "Developers Guide to Architecture of PyGimp" you must reverse engineer.
+From the PyGimp/GimpFu code and the GimpFu (Author's Guide) document.
+
+Start with a primitive gimpfu.py module.
+Test with a null GimpFu plugin (that does nothing really.)
+Proceed to test with other existing GimpFu plugins, say clothify.py
+
+The most basic statement of what the port involves is:
+convert a static binding of Python to Gimp and to a dynamic GI binding.
+That has already been done for many Python plugins that don't use GimpFu.
+The problem is to convert GimpFu to a dynamic binding.
+And what GimpFu does is automate (or provide simplified API) for plugin authors.
+So you take pieces from already ported/authored non-GimpFu Python3/Gimp3 plugins,
+and put those pieces into the GimpFu framework.
+
+
+To use the source
+=================
+The current Gimp 2.99 repository doesn't even attempt to build GimpFu.
+You don't need to hack it so it does (but eventually it should.)
+
+You can just copy the files in the source directory
+into the Python plugin install directory of your dev system.
+(There is no C source to be compiled for GimpFu)
+You can copy a test plugin (that uses GimpFu) from the test directory
+to your user local location for plugins (say ~/.config/gimp/....)
+or to the install location for Gimp packaged Python plugins.
+
+
+
+My dev environment
 ===============
 
 I use Vagga tool for development containers.
+You don't need this if you already can build Gimp.
 
 The vagga.yaml script builds a userspace container,
 and has commands to execute in the container.
@@ -24,11 +75,14 @@ and has commands to execute in the container.
 In other words, the vagga.yaml script completely builds a dev system,
 installing the base OS, all dependencies, and building babl, gegl, and Gimp.
 
-
 Vagga:
     - knows when it needs to rebuild.
     - doesn't require root privileges.
     - Easily deletable (rm -Rf .vagga).
+    - your machine (its original dev environment) is completely isolated.
+    - if you hack at the Gimp clone, the command will rebuild Gimp automatically.
+    - you can pull nightly changes to Gimp and the command will rebuild Gimp automatically.
+
 
 Getting started
 ===============
@@ -46,12 +100,6 @@ And a test.py GimpFu plugin is installed in the container (Gimp menu Somewhere/t
 
 Then you can hack at the .py files in your clone and rerun the last commmand.
 It will take a minute or so to restart (not ideal, probably there is a better way.)
-Advantages:
-   - your machine is completely isolated.
-   - if you hack at the Gimp clone, the command will rebuild Gimp automatically.
-   - you can pull nightly changes to Gimp and the command will rebuild Gimp automatically.
-
-
 
 
 Before you begin creating a dev environment,
@@ -60,10 +108,20 @@ Since the build scripts copy from there into the container.
 So that the container builds Gimp from a local possibly hacked copy.
 I have one minor hack in my Gimp clone to support Gimpfu.
 
+Typically:
+   vaggaGimp (clone of this repository)
+      gimp (you add this clone of gimp 2.99 repository)
+      .vagga (ephemera, ignored, created by vagga, the containers)
+      source (from this repository )
+      vagga.yaml (from this repository)
+      test, readme, etc. (from this repository)
+
+
 The vagga.yaml script copies hacked .py files
 from this repository
-into the right install place e.g. /usr/local/lib/gimp/2.99/python/gimpfu.py
+into the container at the correct install place e.g. /usr/local/lib/gimp/2.99/python/gimpfu.py
 at the last possible moment (after compile Gimp, before running Gimp.)
+
 
 Hack to Gimp itself
 ===================
--- source/gimpfu_pdb.py
+++ source/gimpfu_pdb.py
@@ -3,15 +3,20 @@
 import gi
 gi.require_version("Gimp", "3.0")
 from gi.repository import Gimp
+from gi.repository import GObject    # marshalling
 
 
 class GimpfuPDB():
     '''
     Adaptor to Gimp.PDB
-    GimpFu promises a symbol pdb that acts like the pdb, with attributes of Gimp.PDB.
+    GimpFu defines a symbol "pdb", an instance of this class.
+    Its attributes appear to have similar names as procedures in the Gimp.PDB.
+
+    GimpFu v2 also allowed access using index notation.
+    Obsolete? not supported here yet.
 
     GimpFu creates one instance named "pdb"
-    TODO enforce singleton, but GimpFu user doesn't even imagine this class exists.
+    TODO enforce singleton, but GimpFu plugin author doesn't even imagine this class exists.
 
     FBC
     Problems it solves:
@@ -20,6 +25,8 @@ class GimpfuPDB():
     because Gimp.main() hasn't been called yet
     (which would create the PDB instance and establish self type is GimpPlugin).
     Such code would bind "pdb" to None, forever, since that is the way import works.
+    Besides, the object returned by Gimp.get_pdb() is the PDB manager,
+    not having a method for each procedure IN the PDB.
 
     Alternatively, we could require a plugin's "func" to always first call pdb = Gimp.get_pdb().
     But that is new boilerplate for GimpFu authors, and not backward compatible.
@@ -29,74 +36,112 @@ class GimpfuPDB():
 
     '''
     Implementation:
-    Adaptor pattern.
-    Override special method __get_attribute__.
 
-    OLD to return attribute of Gimp.PDB.
-    In other words, forward attribute access to Gimp.PDB.
-    (when attribute is a function, subsequent invocation is on Gimp.PDB )
+    See:
+    1)"Adaptor pattern"
+    2) Override "special method" __get_attribute__.
+    3) "intercept method calls"
+    4) "marshalling" args.
 
-    NEW Assume each get_attribute is immediately followed by a call.
-    We don't check for callable().
+    An author's access (on right hand side) of an attribute of this class (instance "pdb")
+    is adapted to run a procedure in the PDB.
+    Requires all accesses to attributes of pdb are function calls (as opposed to get data member.)
     Each valid get_attribute() returns an interceptor func.
     The interceptor func marshalls arguments for a call to PDB.run_procedure()
+    and returns result of run_procedure()
 
-    Some get_attributes ARE attributes on Gimp.PDB e.g. run_procedure().
+    ???Some get_attributes ARE attributes on Gimp.PDB e.g. run_procedure().
     We let those pass through, with mangling of procedure name and args?
+
+    Notes:
+
+    Since we override __getattribute__, use idiom to avoid infinite recursion:
+    call super to access self attributes without recursively calling overridden __getattribute__.
+    super is "object".  Alternatively: super().__getattribute__()
 
     Errors:
 
-    Raise if cannot get a reference to Gimp.PDB
+    Exception if cannot get a reference to PDB
     (when this is called before Gimp.main() has been called.)
 
-    No set attribute allowed.
+    Exception if attribute name not a procedure of PDB
+
+    No set attribute should be allowed, i.e. author use pdb on left hand side.
     The PDB (known by alias "pdb") can only be called, it has no data members.
     But we don't warn, and the effect would be to add attributes to an instance of this class.
+    Or should we allow set attribute to mean "store a procedure"?
     '''
 
     def __init__ (self):
         self.adaptee = None
 
-    # @classmethod
-    def _adaptor(self, args):
+
+    def _marshall_args(self, *args):
+        '''
+        Gather many args into Gimp.ValueArray
+        '''
+        marshalled_args = Gimp.ValueArray.new(len(args))
+        index = 0
+        for x in args:
+            # TODO convert to GObject
+            # GObject.Value(GObject.TYPE_STRING, tmp))
+            print("marshall arg")
+            print(type(x))
+
+            # !!! Can't assign GObject to python object: marshalled_arg = GObject.Value(Gimp.Image, x)
+            # ??? I don't understand why insert() doesn't determine the type of its second argument
+
+            marshalled_args.insert(index, GObject.Value(type(x), x))
+            index += 1
+        return marshalled_args
+
+
+    def _adaptor_func(self, *args):
         ''' run a PDB procedure whose name was used like "pdb.name()" e.g. like a method call of pdb object '''
         print ("adaptor called, args", args)
-        # TODO marshall into Gimp.ValueArray
-        Gimp.get_pdb().run_procedure( object.__getattribute__(self, "adapted_proc_name"), args )
+        # !!! Must unpack args before passing to _marshall_args
+        # !!! avoid infinite recursion
+        inner_result = Gimp.get_pdb().run_procedure( object.__getattribute__(self, "adapted_proc_name"),
+                                     object.__getattribute__(self, "_marshall_args")(*args) )
+        # TODO unmarshall result
+        return inner_result
+
 
 
     def  __getattribute__(self, name):
-        # idiomatic avoid infinite recursion when accessing non-forwarded attributes of self
-        # super is "object".  Alternatively: super().__getattribute__()
-        # note adapteeValue is local
-        adapteeValue = object.__getattribute__(self, "adaptee")
-        if adapteeValue is None:
-            # First call, cache empty
-            adapteeValue = Gimp.get_pdb()
-            # cache it
-            self.adaptee = adapteeValue
-            print("adapteeValue: ", adapteeValue)
+        '''
+        override of Python special method
+        Adapts attribute access to become invocation of PDB procedure.
 
-        #  adapteeValue might still be None
+        The usual purpose is to compute the attribute, or get it from an adaptee.
+        '''
 
-        if adapteeValue is None:
-            # TODO better error message
-            # main() calls Gimp.main() to create PDB and establishes GimpFu type is GimpPlugin
+        '''
+        Require that GimpFu plugin author previously called main()
+        which calls Gimp.main() to create PDB and establish GimpFu type is GimpPlugin
+        '''
+        if Gimp.get_pdb() is None:
             raise Exception("Gimpfu: pdb accessed before calling main()")
         else:
             # TODO leave some calls unadapted, direct to PDB
+            # ??? e.g. run_procedure ???, recursion ???
 
-            mangledName = name.replace( '_' , '-')
 
-            if Gimp.get_pdb().procedure_exists(mangledName):
-                # todo return an adaptor function
+
+            # names in PDB have hyphen instead of underbar
+            mangled_proc_name = name.replace( '_' , '-')
+
+            if Gimp.get_pdb().procedure_exists(mangled_proc_name):
+
                 print("return adaptor")
-                # remember state for soon-to-come call of _adaptor
-                self.adapted_proc_name = mangledName
-                return object.__getattribute__(self, "_adaptor")
+                # remember state for soon-to-come call
+                self.adapted_proc_name = mangled_proc_name
+                # return intercept function soon to be called
+                return object.__getattribute__(self, "_adaptor_func")
             else:
                 # TODO print name
                 raise Exception("GimpFu: unknown pdb procedure")
 
+            # OLD
             # will raise AttributeError for names that are not defined by GimpPDB
-            return adapteeValue.__getattribute__(mangledName)
+            # return adapteeValue.__getattribute__(mangled_proc_name)
