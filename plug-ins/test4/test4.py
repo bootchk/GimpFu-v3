860b10e495bc8ea4ec13be4cc32be1bea914cdf3
bootchk <bootch@nc.rr.com>
2020-01-27 18:08:10 -0500

    test4.py tests more __getattr__ cases

--- plug-ins/test4/test4.py
+++ plug-ins/test4/test4.py
@@ -7,9 +7,59 @@ that:
 from gimpfu import *
 
 def plugin_func(image, drawable):
-      print("plugin_func called")
+  print("plugin_func called")
 
+  # expect "AttributeError: since it doesn't exist anywhere"
+  # TODO GimpFu catches AttributeError earlier, we can't catch it
+  """
+  try:
       foo = drawable.bar
+  except AttributeError:
+
+      print ("Pass AttributeError on drawable.bar")
+  """
+
+  '''
+  Access an attribute of adaptee as property
+  (when has_alpha had not been adapted yet)
+  Expect:  gi.FunctionInfo(has_alpha)
+  '''
+  foo = drawable.has_alpha
+  # TODO this is not working: assert isinstance(foo, gi.FunctionInfo)
+  print(foo)
+
+  '''
+  Access an attribute of adaptee as callable.
+  I.E. properly
+  (before has_alpha has been wrapped)
+  # TODO find an attribute that should not be wrapped.
+  Expect: True or False (depends o)
+  '''
+  foo = drawable.has_alpha()
+  assert isinstance(foo, bool)
+  print(foo)
+
+  '''
+  Access an attribute that has been wrapped as a property.
+  Use syntax for a property (no parens)
+  '''
+  foo = drawable.height
+  print(foo)
+  assert isinstance(foo, int)
+
+  '''
+  Access an attribute that has been wrapped as a property.
+  Use syntax for a callable (use parens)
+  Expect: TypeError: 'int' object is not callable
+  '''
+  foo = drawable.height()
+  print(foo)
+
+
+
+
+
+
 
 register(
       "test-missing-drawable-attribute",
