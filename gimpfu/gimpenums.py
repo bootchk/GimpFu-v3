1014d9c8d074207008d8f453953f4a2f7719e91c
bootchk <bootchk@users.noreply.github.com>
2020-02-13 07:11:14 -0500

    TRUE and FALSE deprectated enums

--- gimpfu/gimpenums.py
+++ gimpfu/gimpenums.py
@@ -171,3 +171,36 @@ GIMP_BUCKET_FILL_FG,      /*< desc="FG color fill" >*/
   GIMP_BUCKET_FILL_BG,      /*< desc="BG color fill" >*/
   GIMP_BUCKET_FILL_PATTERN
 '''
+
+
+# TRUE and FALSE still deprecatated, not obsolete
+# start verbatim from v2
+
+# This is from pygtk/gtk/__init__.py
+# Copyright (C) 1998-2003  James Henstridge
+
+class _DeprecatedConstant:
+    def __init__(self, value, name, suggestion):
+        self._v = value
+        self._name = name
+        self._suggestion = suggestion
+
+    def _deprecated(self, value):
+        import warnings
+        message = '%s is deprecated, use %s instead' % (self._name,
+                                                        self._suggestion)
+        warnings.warn(message, DeprecationWarning, 3)
+        return value
+
+    __nonzero__ = lambda self: self._deprecated(self._v == True)
+    __int__     = lambda self: self._deprecated(int(self._v))
+    __str__     = lambda self: self._deprecated(str(self._v))
+    __repr__    = lambda self: self._deprecated(repr(self._v))
+    __cmp__     = lambda self, other: self._deprecated(cmp(self._v, other))
+
+TRUE = _DeprecatedConstant(True, 'gimpenums.TRUE', 'True')
+FALSE = _DeprecatedConstant(False, 'gimpenums.FALSE', 'False')
+
+del _DeprecatedConstant
+
+# end verbatim from v2
