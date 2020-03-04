e1c2b654e16a8fcd7128eb57de5876db1b1b4afc
bootchk <bootchk@users.noreply.github.com>
2020-03-03 19:21:37 -0500

    futz with hello_world.py test plugin

--- plug-ins/hello_world/hello_world.py
+++ plug-ins/hello_world/hello_world.py
@@ -1,6 +1,19 @@
-# lkk omit hash bang specific to OSX   #![path to Gimp application]/GIMP-2.10.app/Contents/MacOS/python
+# lkk omit hash bang specific to OSX
+#        ![path to Gimp application]/GIMP-2.10.app/Contents/MacOS/python
+
+
+'''
+lkk
+As a test case:
+expect "! Hello world written to status bar"
+
+Also, you must open an image.
+As written (image_type=="") without an image, generates an error from Gimp.
+'''
+
 from gimpfu import *
-import gimp
+# lkk commented out: this causes failure?
+# import gimp
 
 def python_message(image, drawable, message):
     gimp.message(message)
