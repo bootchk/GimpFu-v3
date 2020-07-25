5cac1eac11653b9d7c0c9857a43cdeaffcd4bc46
bootchk <bootchk@users.noreply.github.com>
2020-07-25 08:03:46 -0400

    Comments gimpenums.py

--- gimpfu/gimpenums.py
+++ gimpfu/gimpenums.py
@@ -5,6 +5,22 @@ Define Python symbols for Gimp enums.
 These are aliases.
 Convenience for authors.
 
+In GimpFu, enums are named like RGB_IMAGE.
+Capitalized.
+With underbar character.
+
+Examples:
+GimpFu's RGB_IMAGE is an alias for introspected Gimp.ImageType.RGB_IMAGE
+GimpFu's NORMAL_MODE is an alias for Gimp.LayerMode.NORMAL
+   (note that the GimpFu name has suffix "_MODE")
+
+Usually the names used in Gimp are the same as used in GimpFu v2 (except for prefix or suffix).
+But there are some abberations from that rule.
+
+GimpFu enums:
+- most are generated programatically from introspected Gimp enums.
+- some are defined statically, FBC: (sometimes deprecated or obsolete)
+
 TODO Warn about deprecated symbols.
 A deprecated symbol is used by v2 plugins
 but no longer has a corresponding Gimp enum.
@@ -13,12 +29,36 @@ A moot symbol is one used by v2 plugins,
 but only in GimpFu methods that are adapted.
 TODO think about this some more
 
-TODO hack, just the ones reference by clothify, not real values
 
-If nothing else, enumerate them all here.
-Probably should automate it,
-if the names used in Gimp are the same (except for prefix)
-Probably there are some abberations.
+FIXME
+=====
+
+GimpFu v2 see gimpenumsmodule.c
+It creates a Python module named _gimpenums.
+
+It uses PyGObject, has a different code structure.
+This should use that code, just update it to not be C.
+
+
+
+The process for writing this module
+===================================
+1) Generate automagically
+In an API doc for Gimp generated from its .gir:
+scroll to the 'Other section'
+where enums are described.
+(But the section also includes some constants that are not enums.)
+Most enums are name like:
+- Gimp.FooType
+- Gimp.ChannelOps
+- Gimp.FooMode
+For each enum, in this code, insert a call to define_symbols_for_enum(Gimp.MergeType).
+But if you don't have an example from GimpFu v2,
+you won't know whether you need a suffix, prefix, or neither.
+
+2) Hand code
+Search the GimpFu v2 code for abberant enum definitions.
+Insert code to define them.
 '''
 
 import gi
@@ -45,10 +85,10 @@ for each
 
 
 def get_enum_name(enum):
-    ''' Return name of a Gimp enum type. '''
-    '''
+    """ Return long name of the short name of a Gimp enum type.
+
     e.g. short_name is 'MergeType', long_name is 'Gimp.MergeType'
-    '''
+    """
     short_name = enum.__name__
     # Qualify with Gimp namespace
     long_name = "Gimp." + short_name
@@ -104,9 +144,9 @@ FG_BG_RGB_MODE      = 1999
 FG_BUCKET_FILL      = Gimp.FillType.FOREGROUND
 BG_BUCKET_FILL      = Gimp.FillType.BACKGROUND
 
+# Note that even in Gimp2, COLOR_MODE meant HSL_COLOR, not HSV
 SATURATION_MODE = Gimp.LayerMode.HSV_SATURATION
 HUE_MODE        = Gimp.LayerMode.HSV_HUE
-# TODO bug in Gimp?
 COLOR_MODE = Gimp.LayerMode.HSL_COLOR
 VALUE_MODE = Gimp.LayerMode.HSV_VALUE
 
@@ -132,30 +172,41 @@ for attribute in dir(foo):
 
 """
 TODO open an issue with Gimp:
-the naming is inconsistent, not alway FooType, sometimes just Foo.
+Gimp names for enum types is inconsistent, not alway FooType, sometimes just Foo.
 """
 
 define_symbols_for_enum(Gimp.MergeType)
-# ImageBaseType is superset of ImageType, i.e. RGB => RGB, RGBA, etc.
+# Layer merge types
+# FLATTEN_IMAGE = Gimp.MergeType.FLATTEN_IMAGE
+
 define_symbols_for_enum(Gimp.ImageBaseType)
+# ImageBaseType is superset of ImageType, i.e. RGB => RGB, RGBA, etc.
 #RGB                 = Gimp.ImageBaseType.RGB  # GRAY, INDEXED
+
 define_symbols_for_enum(Gimp.ImageType)
 #RGB_IMAGE           = Gimp.ImageType.RGB_IMAGE
+
 define_symbols_for_enum(Gimp.LayerMode, suffix='_MODE')
 #NORMAL_MODE         = Gimp.LayerMode.NORMAL
 #MULTIPLY_MODE       = Gimp.LayerMode.MULTIPLY
+
 define_symbols_for_enum(Gimp.FillType, suffix='_FILL')
 # TODO some wild plugins refer to e.g. FILL_TRANSPARENT, with a prefix
 #BACKGROUND_FILL     = Gimp.FillType.BACKGROUND
 #WHITE_FILL          = Gimp.FillType.WHITE
+
 define_symbols_for_enum(Gimp.ChannelOps, prefix='CHANNEL_OP_')
 #CHANNEL_OP_REPLACE  = Gimp.ChannelOps.REPLACE
+
 define_symbols_for_enum(Gimp.GradientType, prefix='GRADIENT_')
 #GRADIENT_RADIAL     = Gimp.GradientType.RADIAL
+
 define_symbols_for_enum(Gimp.RepeatMode, prefix='REPEAT_')
 #REPEAT_NONE         = Gimp.RepeatMode.NONE
+
 define_symbols_for_enum(Gimp.HistogramChannel, prefix='HISTOGRAM_')
 #HISTOGRAM_VALUE      = Gimp.HistogramChannel.VALUE
+
 define_symbols_for_enum(Gimp.MaskApplyMode, prefix='MASK_')
 #MASK_APPLY           = Gimp.MaskApplyMode.APPLY
 
@@ -166,6 +217,7 @@ define_symbols_for_enum(Gimp.AddMaskType, prefix='ADD_MASK_')
 
 define_symbols_for_enum(Gimp.SizeType)
 # PIXELS = Gimp.SizeType.PIXELS,     POINTS
+
 define_symbols_for_enum(Gimp.HueRange, prefix='HUE_RANGE_')
 # HUE_RANGE_ALL = Gimp.HueRange.ALL
 
@@ -173,11 +225,18 @@ define_symbols_for_enum(Gimp.HueRange, prefix='HUE_RANGE_')
 define_symbols_for_enum(Gimp.DesaturateMode, prefix='DESATURATE_')
 # DESATURATE_LIGHNTESS = Gimp.DesaturateMode.LIGHTNESS
 
-# For mode conversions
+# For image conversions
+
 define_symbols_for_enum(Gimp.ConvertDitherType, prefix='DITHER_')
+# scale conversion
 # DITHER_NONE = Gimp.ConvertDitherType.None
+
 define_symbols_for_enum(Gimp.ConvertPaletteType, prefix='PALETTE_')
+# mode conversion
 # PALETTE_MONO = Gimp.ConvertPaletteType.MONO
+
+define_symbols_for_enum(Gimp.RotationType, prefix='ROTATE_')
+# ROTATE_90 = Gimp.RotationType.90
 
 
 
